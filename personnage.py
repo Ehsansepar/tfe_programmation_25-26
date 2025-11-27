32f9d303d0c469c663ec2459ce016f5fa0736c6d
Ehsansepar <ehsan.separ007@gmail.com>
2025-11-27 20:24:52 +0100

    problem gravity solved

--- personnage.py
+++ personnage.py
@@ -10,14 +10,11 @@ class Personnage:
         self.speed = speed
 
 
-        # self.vitesse_verticale = 0 
-        # self.gravite = 0.5 # en pixels par frame^2
-        # self.puissance_saut = -15 # en pixels par frame
-        # self.is_jumping = False
+        self.vitesse_verticale = 0 
+        self.gravite = 0.5 # en pixels par frame^2
+        self.puissance_saut = -15 # en pixels par frame
+        self.is_jumping = False
 
-        self.en_saut = False
-        self.compteur_saut = 0
-        self.taille_saut = 20
 
     def move(self):
         keys = pygame.key.get_pressed()
@@ -33,36 +30,16 @@ class Personnage:
             self.jump()
 
 
-        # self.vitesse_verticale += self.gravite # Appliquer la gravité a chaque fois il augumente la vitesse verticale
-        # self.y += self.vitesse_verticale
-        # ground_y = HEIGHT - self.height
+        self.vitesse_verticale += self.gravite # Appliquer la gravité a chaque fois il augumente la vitesse verticale
+        self.y += self.vitesse_verticale
+        ground_y = HEIGHT - self.height
 
-        # if self.y >= ground_y:
-        #     self.y = ground_y
-        #     self.vitesse_verticale = 0
-        #     self.is_jumping = False
-
-        if self.en_saut:
-            self.y -= 10
-            self.compteur_saut += 1
-
-            if self.compteur_saut < 0 :
-                self.en_saut = False
-        else : 
-            self.y += 10
-    
-        sol = HEIGHT - self.height
-
-        if self.y >= sol :
-            self.y = sol
+        if self.y >= ground_y:
+            self.y = ground_y
+            self.vitesse_verticale = 0
+            self.is_jumping = False
 
     def jump(self):
-        # if not self.is_jumping:
-        #     self.vitesse_verticale = self.puissance_saut
-        #     self.is_jumping = True
-
-        sol = HEIGHT - self.height
-
-        if self.y >= sol :
-            self.en_saut = True
-            self.compteur_saut = - self.taille_saut
+        if not self.is_jumping:
+            self.vitesse_verticale = self.puissance_saut
+            self.is_jumping = True

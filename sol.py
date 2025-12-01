557ae28bda2b6926778c44694e5de322f78ee1d3
Ehsansepar <ehsan.separ007@gmail.com>
2025-12-01 21:59:11 +0100

    Fix Sol class initialization to use 'sol' parameter and update rectangle attribute

--- sol.py
+++ sol.py
@@ -2,8 +2,6 @@ import pygame
 from config import WIDTH, HEIGHT, FPS
 
 class Sol :
-    def __init__(self) :
-        self.rect = pygame.Rect(0, HEIGHT-100, WIDTH, 20)
+    def __init__(self, sol) :
+        self.sol = pygame.Rect(0, HEIGHT-100, WIDTH, 20)
 
-    def afficher(self, ecran) :
-        ecran.blit(self.rect)

e4d00d6efb27cb50327384aa7d9bd2b8d99d5b14
Ehsansepar <ehsan.separ007@gmail.com>
2025-12-01 20:58:30 +0100

    Fix Sol rectangle dimensions and update rendering method

--- sol.py
+++ sol.py
@@ -3,7 +3,7 @@ from config import WIDTH, HEIGHT, FPS
 
 class Sol :
     def __init__(self) :
-        self.rect = pygame.Rect(0,500, WIDTH, 50)
+        self.rect = pygame.Rect(0, HEIGHT-100, WIDTH, 20)
 
     def afficher(self, ecran) :
-        pygame.draw.rect(ecran, (100, 50, 0), self.rect)
+        ecran.blit(self.rect)

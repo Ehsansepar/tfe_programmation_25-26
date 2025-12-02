379c5360a15a906d86f799aa7bf6c795050c001b
Ehsansepar <ehsan.separ007@gmail.com>
2025-12-02 21:31:45 +0100

    Enhance Gagner class to manage menu and quit interactions; add hover detection for buttons

--- gagner.py
+++ gagner.py
@@ -11,6 +11,13 @@ class Gagner :
         police = pygame.font.SysFont('comicsansms', 40, bold=True)
 
         self.finished_rect = pygame.Rect(WIDTH-100, HEIGHT-150, 50, 100)
+        
+        self.rect_meunu = pygame.Rect(WIDTH // 2 - 100, 400, 200, 50)
+        self.rect_quitter = pygame.Rect(WIDTH // 2 - 100, 500, 200, 50)
+
+        self.couleur_hover = (200, 200, 200)
+        return
+    
 
     def afficher_text(self, text, font, text_col, x, y):
         img = font.render(text, True, text_col)
@@ -35,7 +42,26 @@ class Gagner :
                         return "menu"
                     elif event.key == pygame.K_END:
                         return "quit"
+            
+            mouse_pos = pygame.mouse.get_pos()
 
+            if self.rect_meunu.collidepoint(mouse_pos):
+
+                if pygame.mouse.get_pressed()[0]:
+                    personnage.speed = 5
+                    personnage.x = 100
+                    personnage.y = 300
+                    return "menu"
+                
+            if self.rect_quitter.collidepoint(mouse_pos):
+                if pygame.mouse.get_pressed()[0]:
+                    return "quit"
+                
+                elif not self.rect_meunu.collidepoint(mouse_pos) and not self.rect_quitter.collidepoint(mouse_pos):
+                    print("not hover")
+                    
+            pygame.draw.rect(self.ecran, self.couleur_hover, self.rect_meunu)
+            pygame.draw.rect(self.ecran, self.couleur_hover, self.rect_quitter)
 
             self.ecran.fill((34, 139, 34))
 

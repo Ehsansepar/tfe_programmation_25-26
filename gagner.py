8d938fcd0b047f592b1af234eec4ad9cc44b8611
Ehsansepar <ehsan.separ007@gmail.com>
2025-12-03 20:53:29 +0100

    Refactor Gagner class to streamline menu and quit button handling; add hover effects and debug prints

--- gagner.py
+++ gagner.py
@@ -12,10 +12,10 @@ class Gagner :
 
         self.finished_rect = pygame.Rect(WIDTH-100, HEIGHT-150, 50, 100)
         
-        self.rect_meunu = pygame.Rect(WIDTH // 2 - 100, 400, 200, 50)
-        self.rect_quitter = pygame.Rect(WIDTH // 2 - 100, 500, 200, 50)
+        # self.rect_meunu = pygame.Rect(WIDTH // 2 - 100, 400, 200, 50)
+        # self.rect_quitter = pygame.Rect(WIDTH // 2 - 100, 500, 200, 50)
 
-        self.couleur_hover = (200, 200, 200)
+        # self.couleur_hover = (200, 200, 200)
         return
     
 
@@ -30,37 +30,49 @@ class Gagner :
         running = True
 
         while running:
+
+            mouse_pos = pygame.mouse.get_pos()
+            mouse_clicked = pygame.mouse.get_pressed()
+
+            rect_menu = pygame.Rect(WIDTH // 2 - 100, 400, 200, 50)
+            rect_quitter = pygame.Rect(WIDTH // 2 - 100, 500, 200, 50)
+            
+            
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     return "quit"
 
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_m:
-                        personnage.speed = 5
-                        personnage.x = 100
-                        personnage.y = 300
                         return "menu"
+                    
                     elif event.key == pygame.K_END:
                         return "quit"
-            
-            mouse_pos = pygame.mouse.get_pos()
 
-            if self.rect_meunu.collidepoint(mouse_pos):
-
-                if pygame.mouse.get_pressed()[0]:
-                    personnage.speed = 5
-                    personnage.x = 100
-                    personnage.y = 300
-                    return "menu"
-                
-            if self.rect_quitter.collidepoint(mouse_pos):
-                if pygame.mouse.get_pressed()[0]:
-                    return "quit"
-                
-                elif not self.rect_meunu.collidepoint(mouse_pos) and not self.rect_quitter.collidepoint(mouse_pos):
-                    print("not hover")
-                    
             self.ecran.fill((34, 139, 34))
+
+            couleur_rect_menu = (0, 0, 0)
+            couleur_rect_quitter = (255, 0, 0)
+
+
+            if rect_menu.collidepoint(mouse_pos):
+                couleur_rect_menu = (100, 100, 100)
+                if mouse_clicked[0]:
+                    print("menu clicked from gagner.py")
+                    return "menu"
+            
+            if rect_quitter.collidepoint(mouse_pos):
+                couleur_rect_quitter = (200, 0, 0)
+                if mouse_clicked[0]:
+                    print("quit clicked from gagner.py")
+                    return "quit"
+
+
+                    
+
+            pygame.draw.rect(self.ecran, couleur_rect_menu, rect_menu)
+            pygame.draw.rect(self.ecran, couleur_rect_quitter, rect_quitter)
+
 
 
             self.afficher_text("Félicitations !", police, (255, 215, 0), WIDTH // 2, 150)
@@ -69,4 +81,7 @@ class Gagner :
             self.afficher_text("M - Retour au menu", police, (200, 200, 200), WIDTH // 2, 400)
             self.afficher_text("END - Quitter", police, (240, 113, 103), WIDTH // 2, 500)
 
+            
+            
+            
             pygame.display.flip()

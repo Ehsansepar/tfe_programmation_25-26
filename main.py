d06ed14f3b414e85d48c26ca880fb3fb3b51f401
Ehsansepar <ehsan.separ007@gmail.com>
2025-12-03 22:26:48 +0100

    Refactor main game loop to initialize menu page and handle transitions; integrate parameter settings functionality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from config import WIDTH, HEIGHT, FPS
+from parametre import Parametre
 from personnage import Personnage
 from menu import *
 from gagner import Gagner
@@ -11,6 +12,7 @@ pygame.init()
 ecran = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Mon Jeu Pygame")
 clock = pygame.time.Clock()
+
 
 
 sol = Sol(size=(WIDTH, 20), coulor=(193, 120, 90), pos_x=0, pos_y=HEIGHT-100)
@@ -28,7 +30,7 @@ police = pygame.font.SysFont('Arial', 30)
 
 police_pour_notre_cheat = pygame.font.SysFont('Arial', 20)
 
-page = "game"
+page = "menu"
 running = True
 while running:
     if page == "game" :
@@ -61,6 +63,7 @@ while running:
             afficher_text("Ne triche pas, flemmard ! ", police_pour_notre_cheat, (255, 100, 100), WIDTH // 2 - 150, HEIGHT // 2 - 50)
             afficher_text("c est just pour tester gagner.py j ai flemme de faire tout le temps gagner ! 🤣", police_pour_notre_cheat, (255, 100, 100), WIDTH // 2 - 350, HEIGHT // 2 - 100)
             if souris_est_cliquee[0]:
+                souris_est_cliquee = [0, 0, 0]
                 page = "win"
         # === FIN DU CHEAT CODE ===
 
@@ -92,7 +95,26 @@ while running:
     elif page == "menu" :
         
         menu = Menu(ecran)
-        page = menu.run_menu()
+        result = menu.run_menu()
+        
+        if result == "game":
+            personnage.mettre_a_pos_initiale()
+            page = "game"
+        elif result == "quit":
+            page = "quit"
+        else:
+            page = result
+
+# ------------------------------------------------------------------------------
+
+    
+    elif page == "parametre" :
+        parametre = Parametre(ecran)
+        result = parametre.run_parametre()
+        if result == "menu":
+            page = "menu"
+        elif result == "quit":
+            running = False
 
 
 # ------------------------------------------------------------------------------

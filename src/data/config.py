1c5599485302a07d9f76e8c952cc638cf9804ea5
Ehsansepar <ehsan.separ007@gmail.com>
2026-01-03 23:43:25 +0100

    game optimised for mac and windows users

--- src/data/config.py
+++ src/data/config.py
@@ -1,3 +1,25 @@
 WIDTH = 800
 HEIGHT = 800
-FPS = 150
+
+# son system
+print("=" * 40)
+print("Sur quel système tu joues ?")
+print("1 - Windows")
+print("2 - Mac")
+print("=" * 40)
+choix = input("Ton choix (1 ou 2) : ")
+
+if choix == "2":
+    # Mac - 60 FPS, vitesses plus grandes
+    FPS = 60
+    PLAYER_SPEED = 15
+    PLAYER_GRAVITY = 1.5
+    PLAYER_JUMP = -22
+    print("Mode Mac activé !")
+else:
+    # Windows - peut aller plus vite
+    FPS = 120
+    PLAYER_SPEED = 5
+    PLAYER_GRAVITY = 0.5
+    PLAYER_JUMP = -15
+    print("Mode Windows activé !")

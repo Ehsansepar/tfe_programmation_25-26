abfbade5dbbc3dfa0c633b5802e4c11e5c7aea33
Ehsansepar <ehsan.separ007@gmail.com>
2026-01-10 14:15:18 +0100

    Refactor Inscription class to improve layout and add enter button functionality

--- src/classes/inscription.py
+++ src/classes/inscription.py
@@ -7,15 +7,22 @@ class Inscription :
         
         self.police = pygame.font.Font(None, 40)
         self.user_username = ''
-        self.rect_username = pygame.Rect(233, 311, 353, 44)
+        self.rect_username = pygame.Rect(233, 308, 353, 44)
         self.rect_username_color_border = (255, 255, 255)
 
         self.user_pass = ''
-        self.rect_password = pygame.Rect(233, 370, 353, 44)
+        self.rect_password = pygame.Rect(234, 407, 353, 44)
         self.rect_password_color_border = (255, 255, 255)
         
         self.rect_active_username = False
         self.rect_active_password = False
+    
+
+        self.rect_enter = pygame.Rect(295, 503, 219, 44)
+
+    def afficher_text(self, text, font, text_col, x, y) :
+        img = font.render(text, True, text_col)
+        self.ecran.blit(img, (x, y))
 
     def run_inscription(self, ecran) :
         while True :
@@ -26,20 +33,29 @@ class Inscription :
                 if event.type == pygame.MOUSEBUTTONDOWN :
                     if self.rect_username.collidepoint(event.pos) :
                         self.rect_active_username = True
+                        self.rect_active_password = False
 
                     elif self.rect_password.collidepoint(event.pos) :
                         self.rect_active_password = True
+                        self.rect_active_username = False
+
                     else :
                         self.rect_active_username = False
                         self.rect_active_password = False
 
+                    if self.rect_enter.collidepoint(event.pos) :
+                        print(f"""Username : {self.user_username}\nPassword : {self.user_pass}""")
+                        self.user_username = ''
+                        self.user_pass = ''
+
+                
 
                 
                 if event.type == pygame.KEYDOWN :
                     if event.key == pygame.K_BACKSPACE :
                         if self.rect_active_username :
                             self.user_username = self.user_username[:-1]
-                        
+
                         elif self.rect_active_password :
                             self.user_pass = self.user_pass[:-1]
 
@@ -68,12 +84,19 @@ class Inscription :
             else : 
                 self.rect_password_color_border = (190, 190, 190)
 
+            self.afficher_text("Username", self.police, (255, 255, 255), 236, 286)
             pygame.draw.rect(ecran, self.rect_username_color_border, self.rect_username, 2)
             text_surface = self.police.render(self.user_username, True, (255,255,255))
             ecran.blit(text_surface, (self.rect_username.x + 5, self.rect_username.y + 5))
 
+
+            self.afficher_text("Password", self.police, (255, 255, 255), 237, 382)
             pygame.draw.rect(ecran, self.rect_password_color_border, self.rect_password, 2)
-            text_surface_password = self.police.render(self.user_pass, True, (255, 255, 255))
+            password_masque = '*' * len(self.user_pass)  
+            text_surface_password = self.police.render(password_masque, True, (255, 255, 255))
             ecran.blit(text_surface_password, (self.rect_password.x + 5, self.rect_password.y + 5))
+
+            pygame.draw.rect(ecran, (50, 150, 50), self.rect_enter, 0, 20)
+            self.afficher_text("S'inscrire", self.police, (255, 255, 255), 350, 515)
 
             pygame.display.flip()

70785b9e7ef8502724ba7c516c128861d4717d2f
Ehsansepar <ehsan.separ007@gmail.com>
2026-01-31 23:24:38 +0100

    feat: Implement Pygame window centering for resolution changes and add a "Your resolution" option, accompanied by new documentation.

--- src/classes/parametre.py
+++ src/classes/parametre.py
@@ -1,5 +1,6 @@
 import pygame  
 import data.config as config
+import os
 
 
 pygame.init()
@@ -15,6 +16,8 @@ class Parametre:
         self.son_hover = pygame.mixer.Sound("src/sounds/gta-menu.wav")
         self.son_back = pygame.mixer.Sound("src/sounds/gta-menuOut.wav")
 
+        self.taille = pygame.display.get_desktop_sizes()
+        print(self.taille) 
 
         self.resulo_panel_ouvert = False
 
@@ -38,7 +41,7 @@ class Parametre:
 
 
             # le truc de popup pour les fenetre de reso
-            fenetre_fond = pygame.Rect(config.WIDTH // 2 - 200, 100, 400, 610)
+            fenetre_fond = pygame.Rect(config.WIDTH // 2 - 200, 100, 400, 660) 
             
             
             # ca c est les option on va dire
@@ -50,8 +53,9 @@ class Parametre:
             opt_6 = pygame.Rect(config.WIDTH // 2 - 150, 440, 300, 50)   # 1920x1080
             opt_7 = pygame.Rect(config.WIDTH // 2 - 150, 500, 300, 50)   # 2560x1440
             opt_fullscreen = pygame.Rect(config.WIDTH // 2 - 150, 560, 300, 50) 
+            opt_your_reso = pygame.Rect(config.WIDTH // 2 - 150, 620, 300, 50)
 
-            opt_fermer = pygame.Rect(config.WIDTH // 2 - 150, 640, 300, 50) 
+            opt_fermer = pygame.Rect(config.WIDTH // 2 - 150, 690, 300, 50) 
 
 
 
@@ -73,6 +77,7 @@ class Parametre:
                     if self.resulo_panel_ouvert:
                         if opt_1.collidepoint(event.pos):
                             config.WIDTH, config.HEIGHT = 800, 600
+                            os.environ['SDL_VIDEO_CENTERED'] = '1' # pour centrer la fenetre https://stackoverflow.com/questions/38703791/how-do-i-place-the-pygame-screen-in-the-middle
                             self.ecran = pygame.display.set_mode((config.WIDTH, config.HEIGHT))
                             self.resulo_panel_ouvert = False
 
@@ -82,36 +87,49 @@ class Parametre:
 
                         elif opt_2.collidepoint(event.pos):
                             config.WIDTH, config.HEIGHT = 1024, 768
+                            os.environ['SDL_VIDEO_CENTERED'] = '1' # pour centrer la fenetre
                             self.ecran = pygame.display.set_mode((config.WIDTH, config.HEIGHT))
                             self.resulo_panel_ouvert = False
                             is_fullScreen = False
                         
                         elif opt_3.collidepoint(event.pos):
                             config.WIDTH, config.HEIGHT = 1280, 720
+                            os.environ['SDL_VIDEO_CENTERED'] = '1' # pour centrer la fenetre
                             self.ecran = pygame.display.set_mode((config.WIDTH, config.HEIGHT))
                             self.resulo_panel_ouvert = False
                             is_fullScreen = False
 
                         elif opt_4.collidepoint(event.pos):
                             config.WIDTH, config.HEIGHT = 1366, 768
+                            os.environ['SDL_VIDEO_CENTERED'] = '1' # pour centrer la fenetre    
                             self.ecran = pygame.display.set_mode((config.WIDTH, config.HEIGHT))
                             self.resulo_panel_ouvert = False
                             is_fullScreen = False
 
                         elif opt_5.collidepoint(event.pos):
                             config.WIDTH, config.HEIGHT = 1600, 900
+                            os.environ['SDL_VIDEO_CENTERED'] = '1' # pour centrer la fenetre
                             self.ecran = pygame.display.set_mode((config.WIDTH, config.HEIGHT))
                             self.resulo_panel_ouvert = False
                             is_fullScreen = False
 
                         elif opt_6.collidepoint(event.pos):
                             config.WIDTH, config.HEIGHT = 1920, 1080
+                            os.environ['SDL_VIDEO_CENTERED'] = '1' # pour centrer la fenetre
                             self.ecran = pygame.display.set_mode((config.WIDTH, config.HEIGHT))
                             self.resulo_panel_ouvert = False
                             is_fullScreen = False
 
                         elif opt_7.collidepoint(event.pos):
                             config.WIDTH, config.HEIGHT = 2560, 1440
+                            os.environ['SDL_VIDEO_CENTERED'] = '1'# pour centrer la fenetre
+                            self.ecran = pygame.display.set_mode((config.WIDTH, config.HEIGHT))
+                            self.resulo_panel_ouvert = False
+                            is_fullScreen = False
+                        
+                        elif opt_your_reso.collidepoint(event.pos):
+                            config.WIDTH, config.HEIGHT = self.taille[0]
+                            os.environ['SDL_VIDEO_CENTERED'] = '1'# pour centrer la fenetre
                             self.ecran = pygame.display.set_mode((config.WIDTH, config.HEIGHT))
                             self.resulo_panel_ouvert = False
                             is_fullScreen = False
@@ -119,6 +137,7 @@ class Parametre:
                         elif opt_fullscreen.collidepoint(event.pos) :
                             if is_fullScreen:
                                 # windowed
+                                os.environ['SDL_VIDEO_CENTERED'] = '1' # pour centrer la fenetre
                                 self.ecran = pygame.display.set_mode((config.WIDTH, config.HEIGHT))
                                 is_fullScreen = False
                             else:
@@ -213,15 +232,15 @@ class Parametre:
                 pygame.draw.rect(self.ecran, (52, 152, 219), opt_7, 0, 10)
                 pygame.draw.rect(self.ecran, (255, 255, 255), opt_7, 2, 10)
                 self.afficher_text("2560 x 1440", self.police_option, (255,255,255), config.WIDTH//2, 525)
-
-                # pygame.draw.rect(self.ecran, (52, 152, 219), opt_fullscreen, 0, 10)
-                # pygame.draw.rect(self.ecran, (255, 255, 255), opt_fullscreen, 2, 10)
-                # self.afficher_text("Full Screen", self.police_option, (255, 255, 255), config.WIDTH//2, 585)
                 
+                pygame.draw.rect(self.ecran, (52, 152, 219), opt_your_reso, 0, 10)
+                pygame.draw.rect(self.ecran, (255, 255, 255), opt_your_reso, 2, 10)
+                self.afficher_text("Your resolution", self.police_option, (255, 255, 255), config.WIDTH//2, 645)
+
                 # annulé
-                pygame.draw.rect(self.ecran, (231, 76, 60), opt_fermer,0, 10)
+                pygame.draw.rect(self.ecran, (231, 76, 60), opt_fermer, 0, 10)
                 pygame.draw.rect(self.ecran, (255, 255, 255), opt_fermer, 2, 10)
-                self.afficher_text("Fermer", self.police_option, (255,255,255), config.WIDTH//2, 665)
+                self.afficher_text("Fermer", self.police_option, (255,255,255), config.WIDTH//2, 715)
 
             if self.resulo_panel_ouvert == True and is_fullScreen == False :
                 pygame.draw.rect(self.ecran, (52, 152, 219), opt_fullscreen, 0, 10)

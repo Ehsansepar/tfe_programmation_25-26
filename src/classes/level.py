0655303e65b42160bb22203df2cd572a7f59ffcc
Ehsansepar <ehsan.separ007@gmail.com>
2026-05-06 11:42:05 +0200

    feat: implement level reset functionality and conditional cursor states for locked levels

--- src/classes/level.py
+++ src/classes/level.py
@@ -9,7 +9,6 @@ class Level:
     def __init__(self, ecran):
         self.ecran = ecran
 
-        # Charger l'image de fond
         image_path = os.path.join(os.path.dirname(__file__), "..", "images", "lvls-img.jpg")
         try:
             self.image_fond = pygame.image.load(image_path).convert()
@@ -19,48 +18,97 @@ class Level:
             self.image_fond = pygame.Surface((config.WIDTH, config.HEIGHT))
             self.image_fond.fill((25, 25, 40))
 
-        # Boutons (coordonnées directes alignées en grille 5x2 pour la résolution 1280x720)
         self.rect_level1 = pygame.Rect(270, 220, 100, 80)
         self.rect_level2 = pygame.Rect(430, 220, 100, 80)
         self.rect_level3 = pygame.Rect(590, 220, 100, 80)
         self.rect_level4 = pygame.Rect(750, 220, 100, 80)
-        self.rect_level5 = pygame.Rect(910, 230, 100, 80) # Légèrement plus bas
+        self.rect_level5 = pygame.Rect(910, 230, 100, 80) 
         
-        self.rect_level6 = pygame.Rect(270, 390, 100, 80) # Ligne du bas descendue
+        self.rect_level6 = pygame.Rect(270, 390, 100, 80) 
         self.rect_level7 = pygame.Rect(430, 390, 100, 80)
         self.rect_level8 = pygame.Rect(590, 390, 100, 80)
         self.rect_level9 = pygame.Rect(750, 390, 100, 80)
         self.rect_level10 = pygame.Rect(900, 370, 120, 120)
         
-        self.rect_aller_menu = pygame.Rect(450, 600, 400, 90) # Menu descendu
+        self.rect_aller_menu = pygame.Rect(450, 600, 400, 90) 
 
-
-        # Liste des boutons et de l'action associée
-        self.levels_rects = [
-            (self.rect_level1, "level1"),
-            (self.rect_level2, "level2"),
-            (self.rect_level3, "level3"),
-            (self.rect_level4, "level4"),
-            (self.rect_level5, "level5"),
-            (self.rect_level6, "level6"),
-            (self.rect_level7, "level7"),
-            (self.rect_level8, "level8"),
-            (self.rect_level9, "level9"),
-            (self.rect_level10, "level10"),
-            (self.rect_aller_menu, "menu")
-        ]
 
     def run_level(self):
         clock = pygame.time.Clock()
         running = True
 
+
+        f = open("src/data/data.txt", "r")
+        niveau_max = int(f.read())
+        f.close() 
+
+
         while running:
-            # Changer le curseur en petite main quand on survole un bouton
             mouse_pos = pygame.mouse.get_pos()
-            is_hovering = any(rect.collidepoint(mouse_pos) for rect, action in self.levels_rects)
-            
-            if is_hovering:
+
+            if self.rect_aller_menu.collidepoint(mouse_pos):
                 pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+                
+            elif self.rect_level1.collidepoint(mouse_pos):
+                if 1 <= niveau_max:
+                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+                else:
+                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_NO) # Croix / Interdit
+                    
+            elif self.rect_level2.collidepoint(mouse_pos):
+                if 2 <= niveau_max:
+                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+                else:
+                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_NO)
+                    
+            elif self.rect_level3.collidepoint(mouse_pos):
+                if 3 <= niveau_max:
+                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+                else:
+                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_NO)
+                    
+            elif self.rect_level4.collidepoint(mouse_pos):
+                if 4 <= niveau_max:
+                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+                else:
+                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_NO)
+                    
+            elif self.rect_level5.collidepoint(mouse_pos):
+                if 5 <= niveau_max:
+                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+                else:
+                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_NO)
+                    
+            elif self.rect_level6.collidepoint(mouse_pos):
+                if 6 <= niveau_max:
+                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+                else:
+                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_NO)
+                    
+            elif self.rect_level7.collidepoint(mouse_pos):
+                if 7 <= niveau_max:
+                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+                else:
+                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_NO)
+                    
+            elif self.rect_level8.collidepoint(mouse_pos):
+                if 8 <= niveau_max:
+                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+                else:
+                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_NO)
+                    
+            elif self.rect_level9.collidepoint(mouse_pos):
+                if 9 <= niveau_max:
+                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+                else:
+                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_NO)
+                    
+            elif self.rect_level10.collidepoint(mouse_pos):
+                if 10 <= niveau_max:
+                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+                else:
+                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_NO)
+                    
             else:
                 pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
 
@@ -75,15 +123,54 @@ class Level:
                         return "menu"
                 
                 if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                    for rect, action in self.levels_rects:
-                        if rect.collidepoint(event.pos):
-                            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
-                            return action
-
-            # Affichage de l'image de fond
+                    
+                    if self.rect_aller_menu.collidepoint(event.pos):
+                        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+                        return "menu"
+                        
+                    if self.rect_level1.collidepoint(event.pos) and 1 <= niveau_max:
+                        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+                        return "level1"
+                        
+                    if self.rect_level2.collidepoint(event.pos) and 2 <= niveau_max:
+                        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+                        return "level2"
+                        
+                    if self.rect_level3.collidepoint(event.pos) and 3 <= niveau_max:
+                        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+                        return "level3"
+                        
+                    if self.rect_level4.collidepoint(event.pos) and 4 <= niveau_max:
+                        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+                        return "level4"
+                        
+                    if self.rect_level5.collidepoint(event.pos) and 5 <= niveau_max:
+                        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+                        return "level5"
+                        
+                    if self.rect_level6.collidepoint(event.pos) and 6 <= niveau_max:
+                        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+                        return "level6"
+                        
+                    if self.rect_level7.collidepoint(event.pos) and 7 <= niveau_max:
+                        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+                        return "level7"
+                        
+                    if self.rect_level8.collidepoint(event.pos) and 8 <= niveau_max:
+                        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+                        return "level8"
+                        
+                    if self.rect_level9.collidepoint(event.pos) and 9 <= niveau_max:
+                        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+                        return "level9"
+                        
+                    if self.rect_level10.collidepoint(event.pos) and 10 <= niveau_max:
+                        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+                        return "level10"
+                    
             self.ecran.blit(self.image_fond, (0, 0))
 
-            # (Optionnel) Décommenter ces lignes pour voir les zones de collision des boutons :
+
             # for rect, action in self.levels_rects:
             #     pygame.draw.rect(self.ecran, (255, 0, 0), rect, 2)
 
